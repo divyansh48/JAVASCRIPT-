844684e293837cc4d4bb09fd8829c9d848b918c5
divyansh48 <166692812+divyansh48@users.noreply.github.com>
2024-04-23 07:41:57 +0530

    Add files via upload

---
+++ variables.js
@@ -0,0 +1,11 @@
+console.log("Hello World")
+let a = 67
+console.log(a)
+
+a = "divyansh"
+console.log(a)
+
+// let 8harry = 7
+// this will give error as the name of the variable cannot start with numbers
+// let var = 7
+// cannot use var as the variable name because this is predifined in the javascxript
