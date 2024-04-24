1302fd68a319dc213f111243c2436cfbd3d0815d
divyansh48 <166692812+divyansh48@users.noreply.github.com>
2024-04-24 17:38:17 +0530

    Add files via upload

---
+++ var_let_const.js
@@ -0,0 +1,16 @@
+var a = 45
+var a = "p"
+let b = "harry"
+
+const author = "harry"
+// author = 5 this will give error const variables cannot be updated as well initialised once did
+b = 4
+const harry = 0 
+let c = null
+let d = undefined
+{
+    let b = "this"
+    console.log(b)
+}
+
+console.log(b)
