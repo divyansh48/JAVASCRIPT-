c04529816fcc538d37510203b0c34399d4fa53c0
divyansh48 <166692812+divyansh48@users.noreply.github.com>
2024-04-26 18:20:46 +0530

    Add files via upload

---
+++ primitivedatatype.js
@@ -0,0 +1,12 @@
+let a = null;
+let b = 34;
+let c = Symbol("kunal");
+let d = "divyansh";
+let e = true;
+let f = BigInt("573");
+let g = undefined;
+
+console.log(a,b,c,d,e,f,g);
+
+console.log(typeof a);
+
