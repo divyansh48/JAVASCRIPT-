f4b0ff4077197d13f920d04295381267a0292a19
divyansh48 <166692812+divyansh48@users.noreply.github.com>
2024-04-27 09:11:46 +0530

    Add files via upload

---
+++ objects.js
@@ -0,0 +1,11 @@
+const item = {
+    "Harry" : 34,
+    "Divyansh" : 87,
+    "kunal" : 6,
+    "rohan" : 909
+
+}
+
+console.log(item["Harry"])
+// console.log(item["harry"]) if the key is not there then it will give undifined
+
